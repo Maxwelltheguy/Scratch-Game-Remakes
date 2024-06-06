643ca05a00739d8cabd7a815cb084e9ad87f815a
Maxwell <maxwellmoss332009@gmail.com>
2024-06-06 10:48:11 -0400

    Started work on maze game

---
+++ Project/Scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+
+
+func _on_maze_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_space_shooter_pressed():
+	pass # Replace with function body.
+
+
+func _on_water_chaos_pressed():
+	pass # Replace with function body.
+
+
+func _on_crate_runner_pressed():
+	pass # Replace with function body.
